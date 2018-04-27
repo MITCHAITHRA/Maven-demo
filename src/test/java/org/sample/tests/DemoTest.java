f3dc85e07baf23f0e48b1a2e96050061bb59880e
latha <lathanaveen2016@gmail.com>
2018-04-27 10:40:35 +0530

    first commit

---
+++ src/test/java/org/sample/tests/DemoTest.java
@@ -0,0 +1,14 @@
+package org.sample.tests;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class DemoTest {
+	
+	@Test
+	public void testA()
+	{
+		Reporter.log("hello world",true);
+	}
+
+}
